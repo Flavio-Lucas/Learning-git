cabc27a13941b28577be4631e54b3ec65622fb94
flavio-lucas <flaviolucas389@gmail.com>
2020-03-01 10:47:45 -0300

    chore: altera retorno da rota

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/teste', (req, res) => {
-    res.json({ helo:'World' });
+    res.json({ helo:'World2' });
 })
 
 app.listen(3333);
